48cc65fc54b4e2737e81db78b23352a620416f45
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-08-06 12:09:59 +0400

    file issue

--- src/main/java/az/course/Main.java
+++ src/main/java/az/course/Main.java
@@ -1,33 +1,15 @@
 package az.course;
 
-import az.course.model.Student;
-import az.course.repository.StudentRepository;
-import az.course.repository.StudentRepositoryImpl;
-import java.util.List;
+
+import az.course.service.FileIO;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        StudentRepository st= new StudentRepositoryImpl();
-//        boolean b = st.insertAllStudent(List.of( new Student("qwe", "asd", 16)));
-//        boolean studentDb = st.deleteStudentById(3);
-        System.out.println(st.addStudentWithProcedure(new Student("wxvz", "aaaaa", 9)));
-
-//        studentDb.setYas(34);
-//        studentDb.setDogumTarixi(LocalDate.of(1994,12,10));
-//        boolean b = st.updateStudent(studentDb);
-//
-////        st.getAllStudent().stream().forEach(System.out::println);
-//
-////        Student student= new Student();
-////        student.setAd("Samir");
-////        student.setSoyad("Salamov");
-////        student.setYas(17);
-//        if(b)
-//            System.out.println("telebe elave olundu");
-//        else
-//            System.out.println("olunmadi");
+        FileIO fileIO= new FileIO();
+//        fileIO.readFromFile();
+        fileIO.flush();
 
     }
 }
---
+++ src/main/java/az/course/service/FileIO.java
@@ -0,0 +1,88 @@
+package az.course.service;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+
+public class FileIO {
+
+    public void writeToFile(String str) {
+
+        try (FileWriter fw = new FileWriter("output.txt", true)) {
+            fw.write(str);
+            Writer as = fw.append("AS");
+            System.out.println(as.toString());
+            fw.flush();
+            Writer w = fw.append("W");
+            System.out.println("Writing successful");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void readFromFile() {
+//        String[] array = new String[100];
+        try (var fw = new BufferedReader(new FileReader("output.txt"), 1)) {
+
+            String line;
+            while ((line = fw.readLine()) != null) {
+
+                System.out.println(line);
+            }
+
+//            System.out.println( fw.getEncoding());
+//           fw.read(array);
+//            for (String a:array
+//                 ) {
+//                System.out.println(a);
+//
+//            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readData() {
+        try(var is = new BufferedInputStream(new FileInputStream("output.txt"))) {
+            System.out.println((char) is.read()); // L
+            if (is.markSupported()) {
+                System.out.println("mark  support");
+                is.mark(100); // Marks up to 100 bytes
+                 System.out.println((char) is.read()); // I
+                long skip = is.skip(5);
+                System.out.println((char) is.read()); // O
+                System.out.println((char) is.read()); // O
+                System.out.println((char) is.read()); // O
+                is.reset(); // Resets stream to position before I
+            }
+
+            System.out.println((char) is.read());
+            System.out.println((char) is.read());
+            System.out.println((char) is.read());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        }
+
+
+        public void flush() {
+        try(var is =new FileOutputStream("output.txt")) {
+                for(int i=0; i<1000; i++) {
+                    is.write((char) i);
+                    if(i % 100 == 0) {
+                        is.flush(); }
+                }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        }
+
+
+}
